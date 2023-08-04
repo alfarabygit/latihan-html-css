c14bda5b067d9f72f3d127a7a11b71b2f300936d
Alfaraby Indra <alfarabyindra@gmail.com>
2023-08-04 09:45:16 +0700

    remove navbar script and style

--- script/app4.js
+++
@@ -1,5 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function () {
-  var navBar = document.querySelector(".nav-bar");
-  navBar.classList.toggle("active");
-};
